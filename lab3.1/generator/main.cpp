aec9861f7b5a232f98bd8621025949f6d57830db
adilcka <hustle.adik69@yandex.ru>
2025-05-24 13:13:14 +0300

    improve generator

--- lab3.1/generator/main.cpp
+++ lab3.1/generator/main.cpp
@@ -19,7 +19,6 @@ void GenerateCompiler(const string& input_grammar_path, const string& output_tre
 
     std::unique_ptr<parser::Node> root = parser.TopDownParse(&scanner);
     root->OutputTree(output_file );
-    cout << "Tree saved" << endl;
 
     const auto& program_node = dynamic_cast<const parser::InnerNode&>(*root);
     auto converter_grammar = semantics::ConverterGrammar{};
@@ -33,8 +32,13 @@ int main() {
     try {
         const string& self_grammar_path = "/Users/adilismailov/Desktop/ИУ9-62Б/Компиляторы/lab3.1/generator/example/self_program.txt";
         const string& self_tree_path = "/Users/adilismailov/Desktop/ИУ9-62Б/Компиляторы/lab3.1/generator/output/self_tree.txt";
-        const string& self_table_path = "/Users/adilismailov/Desktop/ИУ9-62Б/Компиляторы/lab3.1/generator/parser/analyzer_table.cpp";
+        const string& self_table_path = "/Users/adilismailov/Desktop/ИУ9-62Б/Компиляторы/lab3.1/generator/src/parser/analyzer_table.cpp";
         GenerateCompiler(self_grammar_path, self_tree_path, self_table_path);
+
+        const string& grammar_path = "/Users/adilismailov/Desktop/ИУ9-62Б/Компиляторы/lab3.1/generator/example/program.txt";
+        const string& tree_path = "/Users/adilismailov/Desktop/ИУ9-62Б/Компиляторы/lab3.1/generator/output/tree.txt";
+        const string& table_path = "/Users/adilismailov/Desktop/ИУ9-62Б/Компиляторы/lab3.1/calculator/src/parser/table.cpp";
+        GenerateCompiler(grammar_path, tree_path, table_path);
     } catch (const std::exception& e) {
         cerr << e.what() << endl;
         return 1;
